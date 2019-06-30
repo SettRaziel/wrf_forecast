34117ae3b31bd6485b2514400961b513a513f5a0
Benjamin Held <me@bheld.eu>
2019-06-30 12:41:42 +0200

    added method and parameter to parameter_repository

--- lib/parameter/parameter_repository.rb
+++ lib/parameter/parameter_repository.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-06-12 10:45:36
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-05-09 16:04:43
+# @Last Modified time: 2019-06-30 12:39:41
 
 # Parent module which holdes the classes dealing with reading and validating
 # the provided input parameters
@@ -19,13 +19,19 @@ module Parameter
     # @param [Array] unflagged_arguments the argument array
     def process_argument(arg, unflagged_arguments)
       case arg
-        when '-d', '--date'
-          create_argument_entry(:date, unflagged_arguments)
+        when *@mapping[:date] then create_argument_entry(:date, unflagged_arguments)
+        when *@mapping[:period] then create_argument_entry(:period, unflagged_arguments)
         when /-[a-z]|--[a-z]+/ then raise_invalid_parameter(arg)
       else
         raise_invalid_parameter(arg)
       end
       nil
+    end
+
+    # method to define the input string values that will match a given paramter symbol
+    def define_mapping
+      @mapping[:date] = ['-d', '--date']
+      @mapping[:period] = ['-p', '--period']
     end
 
   end
