7bceca80576f3a3123f7b3c2d592b85c397b6f12
Benjamin Held <me@bheld.eu>
2019-10-11 21:24:37 +0200

    added wind directions for output text

---
+++ lib/forecast/english/wind/direction.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+# @Author: Benjamin Held
+# @Date:   2019-09-24 12:22:38
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2019-10-11 21:24:08
+
+# As past of the forecast text this module holds string replacements 
+# for the wind directions
+module Direction
+  N  = 'north'
+  S  = 'south'
+  W  = 'west'
+  E  = 'east'
+  NW = 'northwest'
+  NE = 'northeast'
+  SW = 'southwest'
+  SE = 'southeast'
+end
