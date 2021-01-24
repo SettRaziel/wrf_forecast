9f4a74e7e6ff11a42e43d917a3de2df64c745986
Benjamin Held <me@bheld.eu>
2021-01-24 09:00:48 +0100

    synced wind thresholds with conversion formula

--- lib/wrf_forecast/threshold/wind_threshold.rb
+++ lib/wrf_forecast/threshold/wind_threshold.rb
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2020-03-19 13:59:43
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-05-31 17:13:57
+# @Last Modified time: 2021-01-24 08:56:46
 
 module WrfForecast
 
@@ -11,14 +11,14 @@ module WrfForecast
     # This class determines the significant wind thresholds for a forecast day.
     # That means that this class can only work correctly if the data represents a time
     # span of up to 24 hours.
-    # The indicators and thresholds are bases on the climate indicators of the german
+    # The indicators and thresholds are based on the climate indicators of the german
     # (weatherservice)[https://www.dwd.de/DE/wetter/warnungen_aktuell/kriterien/warnkriterien.html]
     # and an additional catecory for medium strong wind:
     # * windy day: the wind speed exceeds 9 m/s or 5 bft
     # * squall day: the wind speed exceeds 14 m/s or 7 bft
     # * storm squall day: the wind speed exceeds 24 m/s or 9 bft
     # * storm day: the wind speed exceeds 28 m/s or 10 bft
-    # * hurricane day: the wind speed exceeds 32 m/s or 11 bft
+    # * hurricane day: the wind speed exceeds 33 m/s or 11 bft
     class WindThreshold < BaseThreshold
 
       private
@@ -26,11 +26,11 @@ module WrfForecast
       # initialization of the required indicators
       def initialize_indicators
         add_indicator(:windy_day, false, "windy day (wind speed exceeds 9 m/s, 33 km/h, 5 bft)")
-        add_indicator(:squall_day, false, "squall day (wind speed exceeds 14 m/s, 50 km/h, 7 bft)")
+        add_indicator(:squall_day, false, "squall day (wind speed exceeds 14 m/s, 51 km/h, 7 bft)")
         add_indicator(:storm_squall_day, false, 
-                      "storm squall day (wind speed exceeds 24 m/s, 89 km/h, 9 bft)")
-        add_indicator(:storm_day, false, "storm day (wind speed exceeds 28 m/s, 104 km/h, 10 bft)")
-        add_indicator(:hurricane_day, false, "hurricane day (wind speed exceeds 32 m/s, 120 km/h, 11 bft)")
+                      "storm squall day (wind speed exceeds 24 m/s, 87 km/h, 9 bft)")
+        add_indicator(:storm_day, false, "storm day (wind speed exceeds 28 m/s, 101 km/h, 10 bft)")
+        add_indicator(:hurricane_day, false, "hurricane day (wind speed exceeds 33 m/s, 119 km/h, 11 bft)")
         nil
       end
 
@@ -42,7 +42,7 @@ module WrfForecast
           change_indicator(:squall_day, true, value > 14.0)
           change_indicator(:storm_squall_day, true, value > 24.0)
           change_indicator(:storm_day, true, value > 28.0)
-          change_indicator(:hurricane_day, true, value > 32.0)
+          change_indicator(:hurricane_day, true, value > 33.0)
         }
         nil
       end
