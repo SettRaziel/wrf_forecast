aeb2766ef33db7ead0bfcfced90d92f9f5a566fb
Benjamin Held <me@bheld.eu>
2021-04-04 20:37:49 +0200

    fill rain thresholds from locale

--- lib/wrf_forecast/threshold.rb
+++ lib/wrf_forecast/threshold.rb
@@ -1,11 +1,6 @@
-#!/usr/bin/ruby
-# @Author: Benjamin Held
-# @Date:   2020-03-19 14:16:53
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-05-11 19:40:53
-
 module WrfForecast
 
+  require "i18n"
   require "wrf_forecast/threshold/base_threshold"
   require "wrf_forecast/threshold/temperature_threshold"
   require "wrf_forecast/threshold/wind_threshold"
