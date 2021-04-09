93fa9c52ff507ab51ed9bd86a241ffde251817ee
Benjamin Held <me@bheld.eu>
2021-04-09 23:21:34 +0200

    fill geo directions from locale

--- lib/wrf_forecast/data/directions.rb
+++ lib/wrf_forecast/data/directions.rb
@@ -1,17 +1,18 @@
-#!/usr/bin/ruby
-# @Author: Benjamin Held
-# @Date:   2020-08-06 19:24:10
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-08-06 19:56:01
+require "i18n"
 
 module WrfForecast
 
+  # Simple data class to map the geographical directions to a string representation
   class Directions
 
+    # initialization
     def initialize
       initialize_directions
     end
 
+    # encapsulating getter to retrieve the direction string to a given direction
+    # @param [Symbol] symbol the symbol representing the direction
+    # @return [String] the string representation of the direction
     def get_direction_string(symbol)
       @directions[symbol]
     end
@@ -24,14 +25,14 @@ module WrfForecast
     # method to initialize the mapping from direction to direction text
     def initialize_directions
       @directions = Hash.new()
-      @directions[:N]  = "north"
-      @directions[:S]  = "south"
-      @directions[:W]  = "west"
-      @directions[:E]  = "east"
-      @directions[:NW] = "northwest"
-      @directions[:NE] = "northeast"
-      @directions[:SW] = "southwest"
-      @directions[:SE] = "southeast"
+      @directions[:N]  = I18n.t("direction.north")
+      @directions[:S]  = I18n.t("direction.south")
+      @directions[:W]  = I18n.t("direction.west")
+      @directions[:E]  = I18n.t("direction.east")
+      @directions[:NW] = I18n.t("direction.northwest")
+      @directions[:NE] = I18n.t("direction.northeast")
+      @directions[:SW] = I18n.t("direction.southwest")
+      @directions[:SE] = I18n.t("direction.southeast")
       nil
     end
   end
