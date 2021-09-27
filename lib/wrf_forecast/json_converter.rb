0ca1359438365616127ecb2d81790ea95bcca61b
Benjamin Held <me@bheld.eu>
2021-09-27 22:09:26 +0200

    add json converter for hourly results

--- lib/wrf_forecast/json_converter.rb
+++ lib/wrf_forecast/json_converter.rb
@@ -3,6 +3,7 @@ module WrfForecast
   module JsonConverter
     require 'wrf_library/json_converter'
     require 'wrf_forecast/json_converter/forecast_station_converter'
+    require 'wrf_forecast/json_converter/hourly_forecast_converter'
     require 'wrf_forecast/json_converter/text_forecast_converter'
   end
 
