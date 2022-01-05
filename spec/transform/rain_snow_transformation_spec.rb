4f42316346f0b1432ff49c81c83ae5c9939f7873
Benjamin Held <me@bheld.eu>
2022-01-05 20:23:07 +0100

    add test cases for invalid temperatures

--- spec/transform/rain_snow_transformation_spec.rb
+++ spec/transform/rain_snow_transformation_spec.rb
@@ -4,7 +4,7 @@ require "wrf_forecast/transform"
 describe WrfForecast::RainSnowTransformation do
 
   describe "#rain_to_snow" do
-    context "given an rain amount and a temperature" do
+    context "given a rain amount and a valid temperature" do
       it "transform and return the corresponding snow amount" do
         expect(WrfForecast::RainSnowTransformation.rain_to_snow(1, 273.15 )).to eq(10)
       end
@@ -12,7 +12,7 @@ describe WrfForecast::RainSnowTransformation do
   end
 
   describe "#rain_to_snow" do
-    context "given an rain amount and a temperature" do
+    context "given a rain amount and a valid temperature" do
       it "transform and return the corresponding snow amount" do
         expect(WrfForecast::RainSnowTransformation.rain_to_snow(1, 265.15 )).to eq(20)
       end
@@ -20,15 +20,23 @@ describe WrfForecast::RainSnowTransformation do
   end
 
   describe "#rain_to_snow" do
-    context "given an rain amount and a temperature" do
+    context "given a rain amount and a valid temperature" do
       it "transform and return the corresponding snow amount" do
         expect(WrfForecast::RainSnowTransformation.rain_to_snow(1, 246.15 )).to eq(50)
       end
     end
   end
 
+    describe "#rain_to_snow" do
+    context "given a rain amount and an invalid temperature" do
+      it "try transform and return nil" do
+        expect(WrfForecast::RainSnowTransformation.rain_to_snow(1, 286.15 )).to eq(nil)
+      end
+    end
+  end
+
   describe "#snow_to_rain" do
-    context "given an snow amount and a temperature" do
+    context "given a snow amount and a valid temperature" do
       it "transform and return the corresponding rain amount" do
         expect(WrfForecast::RainSnowTransformation.snow_to_rain(15, 268.15 )).to eq(1)
       end
@@ -36,7 +44,7 @@ describe WrfForecast::RainSnowTransformation do
   end
 
   describe "#snow_to_rain" do
-    context "given an snow amount and a temperature" do
+    context "given a snow amount and a valid temperature" do
       it "transform and return the corresponding rain amount" do
         expect(WrfForecast::RainSnowTransformation.snow_to_rain(40, 257.15 )).to eq(1)
       end
@@ -44,10 +52,19 @@ describe WrfForecast::RainSnowTransformation do
   end
 
   describe "#snow_to_rain" do
-    context "given an snow amount and a temperature" do
+    context "given a snow amount and a valid temperature" do
       it "transform and return the corresponding rain amount" do
         expect(WrfForecast::RainSnowTransformation.snow_to_rain(100, 234.15 )).to eq(1)
       end
     end
   end
+
+  describe "#snow_to_rain" do
+    context "given a snow amount and an invalid temperature" do
+      it "try transform and return nil" do
+        expect(WrfForecast::RainSnowTransformation.snow_to_rain(100, 284.15 )).to eq(nil)
+      end
+    end
+  end
+
 end
