f0a17465cba2c52bf8bc2624bbae44b11016415b
Benjamin Held <me@bheld.eu>
2025-03-09 16:44:44 +0100

    separate wind speed and wind direction in json output

--- lib/wrf_forecast/json_converter/text_forecast_converter.rb
+++ lib/wrf_forecast/json_converter/text_forecast_converter.rb
@@ -9,24 +9,34 @@ module WrfForecast
       private
 
       # method to create the output hash for the temperature values
+      # @return [Array] the array with the hourly values    
       def generate_temperature_values
         extreme_values = @forecast.extreme_values[:air_temperature]
         return { :minimum => extreme_values.minimum.round(3), 
                  :maximum => extreme_values.maximum.round(3) }
       end
 
-      # method to create the output hash for the wind values
-      def generate_wind_values
+      # method to create the output hash for the wind speed values
+      # @return [Array] the array with the hourly values      
+      def generate_windspeed_values
         extreme_values = @forecast.extreme_values[:wind_speed]
-        prevalent_direction = WrfForecast::Directions.new().get_direction_string(@forecast.prevalent_direction)
         values = Hash.new()
         values[:minimum] = extreme_values.minimum.round(3)
         values[:maximum] = extreme_values.maximum.round(3)
+        return values
+      end
+
+      # method to create the output hash for wind direction values
+      # @return [Array] the array with the hourly values      
+      def generate_winddirection_values
+        prevalent_direction = WrfForecast::Directions.new().get_direction_string(@forecast.prevalent_direction)
+        values = Hash.new()
         values[:prevalent_direction] = prevalent_direction
         return values
       end
 
       # method to create the output hash for the precipitation values
+      # @return [Array] the array with the hourly values      
       def generate_rain_values
         extreme_values = @forecast.extreme_values[:rain]
         values = Hash.new()
