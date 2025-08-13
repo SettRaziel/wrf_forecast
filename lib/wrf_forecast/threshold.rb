dd9594a43a5e5b64c4bb466033869ba2933f934d
Benjamin Held <me@bheld.eu>
2025-08-13 17:44:27 +0200

    add tests for pressure threshold

--- lib/wrf_forecast/threshold.rb
+++ lib/wrf_forecast/threshold.rb
@@ -2,6 +2,7 @@ module WrfForecast
 
   require "i18n"
   require "wrf_forecast/threshold/base_threshold"
+  require "wrf_forecast/threshold/pressure_threshold"
   require "wrf_forecast/threshold/temperature_threshold"
   require "wrf_forecast/threshold/wind_threshold"
   require "wrf_forecast/threshold/rain_threshold"
